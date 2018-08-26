ae4160a947568e2830666aa017a8c96258a8716c
Yietion <549166802@qq.com>
2018-08-26 10:29:23 +0000

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserAddress;
+use App\Http\Requests\UserAddressRequest;
 
 class UserAddressesController extends Controller
 {
@@ -12,5 +14,26 @@ class UserAddressesController extends Controller
         return view('user_addresses.index', [
             'addresses' => $request->user()->addresses
         ]);
+    }
+    
+    public function create()
+    {
+        return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
+    }
+    
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+            
+        ]));
+        
+        return redirect()->route('user-addresses.index');
     }
 }
