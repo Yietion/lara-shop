9faa4855c8e6b9437e7d0c2b88ee2dc1e28b4428
Yietion <549166802@qq.com>
2018-08-26 11:57:10 +0000

    初始化后台管理

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -8,7 +8,7 @@ class UserAddress extends Model
 {
     //
     protected $fillable = [
-        'province', 'city', 'district', 'address', 'zip', 'contact_name', 'contact_phone', 'last_used_at'
+        'province', 'city', 'district', 'address',  'zip', 'contact_name', 'contact_phone', 'last_used_at'
         
     ];
     protected $dates = ['last_used_at'];
