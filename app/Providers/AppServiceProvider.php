5df7458207dfb56622c9a96b41d00f5331bf4584
Yietion <549166802@qq.com>
2018-08-30 14:47:36 +0000

    引入支付库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +27,26 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->singleton('alipay', function (){
+        	$config = config('pay.alipay');
+        	if(app()->environment() !== "production"){
+        		$config['mode'] = "dev";
+        		$config['log']['level'] = Logger::DEBUG;
+        	}else {
+        		$config['log']['level'] = Logger::WARNING;
+        	}
+        	return Pay::alipay($config);
+        });
+        
+        $this->app->singleton('wechat_pay', function () {
+        	$config = config('pay.wechat');
+        	if (app()->environment() !== 'production') {
+        		$config['log']['level'] = Logger::DEBUG;
+        	} else {
+        		$config['log']['level'] = Logger::WARNING;
+        	}
+        	// 调用 Yansongda\Pay 来创建一个微信支付对象
+        	return Pay::wechat($config);
+        });        	
     }
 }
