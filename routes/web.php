86aba833b37e48840364f46d9e199e3da93c7129
Yietion <549166802@qq.com>
2018-09-11 15:02:22 +0000

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,8 @@ Route::group(['middleware'=>'auth'], function(){
     	Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
         Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+    	//优惠券相关
+        Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     });
 });
 	Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
